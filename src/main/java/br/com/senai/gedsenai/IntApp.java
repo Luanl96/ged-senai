ca71f7c05d0e4fdcf4a867faa3ce0b4ab6d40aca
Luan Lopes <luancordova20@gmail.com>
2022-03-25 22:23:44 -0300

    Primeiro commit

---
+++ src/main/java/br/com/senai/gedsenai/IntApp.java
@@ -0,0 +1,12 @@
+package br.com.senai.gedsenai;
+
+import br.com.senai.gedsenai.view.TelaDeLogin;
+
+public class IntApp {
+
+	public static void main(String[] args) {
+		
+		new TelaDeLogin().logar();
+
+	}
+}
